0de7dc24f0808f1634633dcf2a84b57ddd36541f
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-09-17 01:46:23 +0100

    :bento: add kodama assets and simple sprite

--- src/systems/render_kodama.c
+++ src/systems/render_kodama.c
@@ -4,5 +4,8 @@
 void system_render_kodama(KodamaState * state)
 {
     move_sprite(1,state->x,state->y);
+    move_sprite(2,state->x+8,state->y);
+    move_sprite(3,state->x,state->y+16);
+    move_sprite(4,state->x+8,state->y+16);
 }
 
