a8c2e364b9b804bdb31786330b523f9528f93ff8
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-09-21 23:04:34 +0100

    :alembic: thoughts for rendering implementation

--- src/systems/render_sunsprite.c
+++ src/systems/render_sunsprite.c
@@ -1,6 +1,19 @@
 #include "../../include/systems/render_sunsprite.h"
 #include <gb/gb.h>
 
+// implicit 3 frames per animation
+
+extern unsigned char sunsprite_normal_frames [];
+extern unsigned char sunsprite_dying_frames [];
+extern unsigned char sunsprite_channeling_frames [];
+
+typedef struct 
+{
+    unsigned short ticks;
+    unsigned short current_frame;
+} InstanceRenderData;
+// Set up parrallel Instance Render Data array 
+
 void system_render_sunsprite(SunspriteInstance * instances){
     for(unsigned short i = 0; i < SUNSPRITE_MAX_SPRITES; i++)
     {
