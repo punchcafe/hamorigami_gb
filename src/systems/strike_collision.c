57e12e12134b4661e8fef8ec2519db75f0384331
Luca Fruzza <lfruzza@hotmail.co.uk>
2022-09-24 21:58:17 +0100

    :sparkles: make hitbox accurate(ish)

--- src/systems/strike_collision.c
+++ src/systems/strike_collision.c
@@ -2,18 +2,16 @@
 
 #define SPRITE_HALF_HEIGHT 8
 #define SPRITE_HALF_WIDTH 8
-#define KODAMA_SPRITE_MIN_Y_DIST_NO_COLLISION KODAMA_HALF_HEIGHT + SPRITE_HALF_HEIGHT
-#define KODAMA_SPRITE_MIN_X_DIST_NO_COLLISION KODAMA_HALF_WIDTH + SPRITE_HALF_WIDTH
+#define KODAMA_SPRITE_MIN_Y_DIST_NO_COLLISION 8 + SPRITE_HALF_HEIGHT
+#define KODAMA_SPRITE_MIN_X_DIST_NO_COLLISION 4 + SPRITE_HALF_WIDTH
 
-unsigned char _system_strike_collision_check(Vector * position, KodamaState * kodama_state)
+unsigned char _system_strike_collision_check(Vector * position, int hitbox_x_center, int hitbox_y_center)
 {
-    // TODO: error, use hitbox instead
-    // NOTE! here we rely on the implicit fact that rain sprites and sun sprites are the same size.
-    int x_sep = vec_x(position) - vec_x(&kodama_state->position);
+    int x_sep = vec_x(position) - hitbox_x_center;
     if(x_sep < 0) x_sep = x_sep * -1;
     if(x_sep < KODAMA_SPRITE_MIN_X_DIST_NO_COLLISION)
     {
-        int y_sep = vec_y(position) - vec_y(&kodama_state->position);
+        int y_sep = vec_y(position) - hitbox_y_center;
         if(y_sep < 0) y_sep = y_sep * -1;
         if(y_sep < KODAMA_SPRITE_MIN_Y_DIST_NO_COLLISION)
         {
@@ -23,19 +21,36 @@ unsigned char _system_strike_collision_check(Vector * position, KodamaState * ko
     return 0x00;
 }
 
+void _system_strike_collision_kodama_hitbox_center(KodamaState * kodama_state, Vector * out)
+{
+    // Kodama position is top left postion of the kodama sprite
+    out->y = (vec_y(&kodama_state->position) + 12); // need to ensure no overflow here
+    if(kodama_state->facing == K_F_LEFT)
+    {
+        out->x = (vec_x(&kodama_state->position) - 4); // need to ensure no overflow here
+    }  else {
+        // Facing right
+        out->x = (vec_x(&kodama_state->position) + 22); // 16 + 4, again ensure no overflow
+    }
+}
+
 
 void system_strike_collision(KodamaState * kodama_state, 
                             SunspriteInstance * sunsprites, 
                             RainspriteInstance * rainsprites)
 {
     // If need be, can put this in the individual sunsprite behaviour functions for performance
+    Vector hitbox_center;
+    _system_strike_collision_kodama_hitbox_center(kodama_state, &hitbox_center);
+    int hitbox_x_center = hitbox_center.x;
+    int hitbox_y_center = hitbox_center.y;
     for(unsigned short i=0; i<SUNSPRITE_MAX_SPRITES; i++)
     {
         if(sunsprites[i].alive)
         {
             if(kodama_state->is_striking)
             {
-                if(_system_strike_collision_check(&sunsprites[i].state.position, kodama_state))
+                if(_system_strike_collision_check(&sunsprites[i].state.position, hitbox_x_center, hitbox_y_center))
                 {
                     sunsprites[i].state.state = S_S_DYING;
                     sunsprites[i].state.explosion_countdown = 0;
@@ -49,7 +64,7 @@ void system_strike_collision(KodamaState * kodama_state,
         {
             if(kodama_state->is_striking)
             {
-                if(_system_strike_collision_check(&rainsprites[i].state.position, kodama_state))
+                if(_system_strike_collision_check(&rainsprites[i].state.position, hitbox_x_center, hitbox_y_center))
                 {
                     rainsprites[i].state.state = R_S_DYING;
                     rainsprites[i].state.counter = 0;
