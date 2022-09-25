f4f5e62a44dfee1d968f44dfc66fe446af7da4e9
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-09-25 14:42:39 +0100

    :alembic: experiment with spawn waves

--- src/data/sprite_factory_commands.c
+++ src/data/sprite_factory_commands.c
@@ -1,19 +1,32 @@
 #include"../../include/systems/sprite_factory.h"
 
 const SpriteFactoryCommand sprite_commands [] = {
+     // tutorial wave
+    {20, 0, S_T_SUN, 0},
+    {70, 0, S_T_RAIN, 10},
+     // 1st wave
+    {70, 0, S_T_SUN, 10},
+    {70, 0, S_T_SUN, 0},
+    {20, 0, S_T_RAIN, 0},
+    {20, 0, S_T_SUN, 5},
     {20, 0, S_T_SUN, 0},
     {70, 0, S_T_SUN, 5},
+     // 2nd wave
+    {20, 0, S_T_RAIN, 20},
+    {20, 0, S_T_RAIN, 0},
     {20, 0, S_T_RAIN, 0},
     {20, 0, S_T_SUN, 5},
     {20, 0, S_T_SUN, 0},
     {20, 0, S_T_SUN, 0},
     {70, 0, S_T_SUN, 5},
+     // 3rd wave
+    {20, 0, S_T_RAIN, 25},
     {20, 0, S_T_RAIN, 0},
+    {20, 0, S_T_RAIN, 0},
+    {70, 0, S_T_SUN, 5},
+    {20, 0, S_T_RAIN, 10},
     {20, 0, S_T_SUN, 5},
     {20, 0, S_T_SUN, 0},
-    {20, 0, S_T_SUN, 0},
-    {70, 0, S_T_SUN, 5},
-    {20, 0, S_T_RAIN, 0},
     {20, 0, S_T_SUN, 5},
     {20, 0, S_T_SUN, 0}
 };
